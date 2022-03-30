4ba4f6c16e3791b56bda456c0b177013c1de40e1
cparradoc <cparradoc@minsait.com>
2022-03-30 15:53:53 +0200

    basic classes

--- ts-intro/curso-angular-intro-typescript-ecmascript-1.0.1/src/index.ts
+++ ts-intro/curso-angular-intro-typescript-ecmascript-1.0.1/src/index.ts
@@ -2,4 +2,13 @@
     ===== Código de TypeScript =====
 */
 
+class Heroe {
+    alterEgo: string; //private - solo visible en esta clase
+    edad: number; //public - visible fuera de esta clase
+    nombreReal: string; //static - se puede acceder a su valor sin crear una instancia (Heroe.nombreReal)
+}
+
+const ironman = new Heroe();
+
+console.log(ironman);
 
