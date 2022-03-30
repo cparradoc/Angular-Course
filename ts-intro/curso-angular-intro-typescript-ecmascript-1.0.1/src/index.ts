de717a4a373f56850e7fe327cb2496144e4073c8
cparradoc <cparradoc@minsait.com>
2022-03-30 15:59:08 +0200

    class constructor

--- ts-intro/curso-angular-intro-typescript-ecmascript-1.0.1/src/index.ts
+++ ts-intro/curso-angular-intro-typescript-ecmascript-1.0.1/src/index.ts
@@ -3,12 +3,18 @@
 */
 
 class Heroe {
-    alterEgo: string; //private - solo visible en esta clase
-    edad: number; //public - visible fuera de esta clase
-    nombreReal: string; //static - se puede acceder a su valor sin crear una instancia (Heroe.nombreReal)
+    //alterEgo: string; //private - solo visible en esta clase
+    //edad: number; //public - visible fuera de esta clase
+    //nombreReal: string; //static - se puede acceder a su valor sin crear una instancia (Heroe.nombreReal)
+
+    constructor(
+        public alterEgo: string, 
+        public edad: number, 
+        public nombreReal: string
+        ) {}
 }
 
-const ironman = new Heroe();
+const ironman = new Heroe('Ironman', 45, 'Tony Stark');
 
 console.log(ironman);
 
