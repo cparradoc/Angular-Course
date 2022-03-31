cfe82edb46fb13501e34d2bb481c8848a0cdd531
cparradoc <cparradoc@minsait.com>
2022-03-31 16:22:35 +0200

    @input for children parent communication

--- Angular/01-bases/src/app/dbz/personajes/personajes.component.ts
+++ Angular/01-bases/src/app/dbz/personajes/personajes.component.ts
@@ -1,10 +1,13 @@
-import { Component} from '@angular/core';
+import { Component, Input} from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
 
 @Component({
   selector: 'app-personajes',
   templateUrl: './personajes.component.html'
 })
 export class PersonajesComponent{
-  personajes: any[] = [];
+  
+
+  @Input() personajes: Personaje[] = [];
 
 }
