c1b1e7490a769fb55ddee1d539afa95b833f079f
cparradoc <cparradoc@minsait.com>
2022-03-31 15:34:54 +0200

    ngModel, two way data binding

--- Angular/01-bases/src/app/dbz/main-page/main-page.component.ts
+++ Angular/01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,11 +1,21 @@
 import { Component} from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
+
+  nuevo: Personaje= {
+    nombre: 'Trunks',
+    poder: 14000
+  }
 
   agregar() {
     console.log('hey');
