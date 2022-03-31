a7d4c6161d2c0826fab29311aed4fe39c55926bb
cparradoc <cparradoc@minsait.com>
2022-03-31 18:26:23 +0200

    centralize chracater access from service

--- Angular/01-bases/src/app/dbz/main-page/main-page.component.ts
+++ Angular/01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -10,26 +10,12 @@ import { DbzService } from '../services/dbz.service';
 })
 export class MainPageComponent {
 
-  personajes:Personaje[] = [
-    {
-      nombre: 'Goku',
-      poder: 15000
-    },
-    {
-      nombre: 'Vegeta',
-      poder: 7500
-    }
-  ];
-
   nuevo: Personaje= {
     nombre: 'Maestro Roshi',
     poder: 1000
   }
 
-  agregarNuevoPersonaje(argumento: Personaje) {
-    this.personajes.push(argumento);
-  }
 
-  constructor( private dbzService: DbzService) {}
+  constructor() {}
 
 }
