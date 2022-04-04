736539db9b2c246d943aad160ca9b283026eb906
cparradoc <cparradoc@minsait.com>
2022-04-04 14:09:10 +0200

    show country with switchmap

--- Angular/03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ Angular/03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -14,12 +15,21 @@ export class VerPaisComponent implements OnInit {
               private paisService: PaisService) { } //para suscribirnos a cualquier cambio de la url
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(({id}) => {
-      this.paisService.getPaisPorId(id)
-        .subscribe(pais => {
 
-        });
-    });
+    this.activatedRoute.params
+        .pipe(switchMap(({id}) => this.paisService.getPaisPorId(id)))
+        .subscribe(resp => {
+          console.log(resp);
+        })
+
+
+    //Mismo resultado pero de otra forma
+    // this.activatedRoute.params.subscribe(({id}) => {
+    //   this.paisService.getPaisPorId(id)
+    //     .subscribe(pais => {
+    //       console.log(pais)
+    //     });
+    // });
   }
 
 }
