bd0e24975f5d5fa3e16a6944581b993454703ade
cparradoc <cparradoc@minsait.com>
2022-04-04 14:20:08 +0200

    country info

--- Angular/03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ Angular/03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais-interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -11,16 +12,17 @@ import { PaisService } from '../../services/pais.service';
 })
 export class VerPaisComponent implements OnInit {
 
+  pais!: Country;
+
   constructor(private activatedRoute: ActivatedRoute,
               private paisService: PaisService) { } //para suscribirnos a cualquier cambio de la url
 
   ngOnInit(): void {
 
     this.activatedRoute.params
-        .pipe(switchMap(({id}) => this.paisService.getPaisPorId(id)))
-        .subscribe(resp => {
-          console.log(resp);
-        })
+        .pipe(switchMap(({id}) => this.paisService.getPaisPorId(id)),
+            tap(console.log))
+        .subscribe(pais => this.pais = pais)
 
 
     //Mismo resultado pero de otra forma
