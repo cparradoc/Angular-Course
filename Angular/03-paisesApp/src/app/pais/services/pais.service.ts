2100c6355ee4ab059fafc09406750b3e2ea2b410
cparradoc <cparradoc@minsait.com>
2022-04-04 13:41:25 +0200

    search by capital city

--- Angular/03-paisesApp/src/app/pais/services/pais.service.ts
+++ Angular/03-paisesApp/src/app/pais/services/pais.service.ts
@@ -8,13 +8,21 @@ import { Country } from '../interfaces/pais-interface';
 })
 export class PaisService {
 
-  private apiUrl: string = 'https://restcountries.com/v2'
+  private apiUrl: string = 'https://restcountries.com/v2';
 
   constructor(private http: HttpClient) { }
 
   buscarPais(termino: string): Observable<Country[]> {
 
     const url = `${this.apiUrl}/name/${termino}`
+
+    return this.http.get<Country[]>(url);
+
+  }
+
+  buscarCapitalPais(termino: string): Observable<Country[]> {
+
+    const url = `${this.apiUrl}/capital/${termino}`
 
     return this.http.get<Country[]>(url);
 
