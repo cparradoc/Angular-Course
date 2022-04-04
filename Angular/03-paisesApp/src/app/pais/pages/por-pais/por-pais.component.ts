d3200b0ed60579a42aa7b03d779278ff9d5e0961
cparradoc <cparradoc@minsait.com>
2022-04-04 11:56:41 +0200

    error handling

--- Angular/03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ Angular/03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -10,17 +10,21 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent {
 
   termino: string = '';
+  hayError: boolean = false;
 
 
   constructor(private paisService: PaisService) { }
 
   
   buscar() {
+    this.hayError = false;
     console.log(this.termino);
 
     this.paisService.buscarPais(this.termino)
     .subscribe(respuesta => {
       console.log(respuesta);
+    }, (err) => {
+      this.hayError = true;
     })
   }
 
