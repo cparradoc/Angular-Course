307853b8371c3ec529e50f10c180a0a51ef7d716
cparradoc <cparradoc@minsait.com>
2022-04-04 17:47:11 +0200

    show countries by region

--- Angular/03-paisesApp/src/app/pais/pages/por-region/por-region.component.ts
+++ Angular/03-paisesApp/src/app/pais/pages/por-region/por-region.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais-interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
@@ -15,9 +17,9 @@ export class PorRegionComponent {
 
   regiones: string[] = ['EU', 'EFTA', 'CARICOM', 'PA', 'AU', 'USAN', 'EEU', 'AL', 'ASEAN', 'CAIS', 'CEFTA', 'NAFTA', 'SAARC'];
   regionActiva: string = ''
-  //https://restcountries.com/v3.1/region/
+  paises: Country[] = [];
 
-  constructor() { }
+  constructor(private paisService: PaisService) { }
 
   getClaseCSS(region: string) {
     return (region === this.regionActiva) ? 'btn btn-primary' : 'btn bton-outline-primary';
@@ -26,8 +28,13 @@ export class PorRegionComponent {
   activarRegion(region: string) {
     this.regionActiva = region;
 
-    //hacer llamado al servicio
+    this.paisService.buscarRegion(region)
+    .subscribe(paises => {
+      this.paises = paises;
+    });
   }
+
+
 
   
 }
