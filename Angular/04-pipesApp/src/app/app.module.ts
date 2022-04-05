de21b626bcf039b425aaebd2b5b3b2323c26a677
cparradoc <cparradoc@minsait.com>
2022-04-05 13:55:57 +0200

    timezones and other languages

--- Angular/04-pipesApp/src/app/app.module.ts
+++ Angular/04-pipesApp/src/app/app.module.ts
@@ -10,9 +10,11 @@ import { VentasModule } from './ventas/ventas.module';
 
 //Cambiar el locale de la app
 import localeEs from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
 import { registerLocaleData } from '@angular/common'
 
 registerLocaleData(localeEs);
+registerLocaleData(localeFr);
 
 
 @NgModule({
