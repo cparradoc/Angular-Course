60ead33bd28e03e891b6b6bbbc2d6494e8e6d9bb
cparradoc <cparradoc@minsait.com>
2022-04-05 13:38:51 +0200

    change default language

--- Angular/04-pipesApp/src/app/app.module.ts
+++ Angular/04-pipesApp/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -7,6 +7,12 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { AppRouterModule } from './app-router.module';
 import { VentasModule } from './ventas/ventas.module';
+
+//Cambiar el locale de la app
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common'
+
+registerLocaleData(localeEs);
 
 
 @NgModule({
@@ -19,7 +25,9 @@ import { VentasModule } from './ventas/ventas.module';
     AppRouterModule,
     VentasModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
